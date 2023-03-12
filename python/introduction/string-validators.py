c2d272b9da4fa1c5508a12fa0877030e8d9ed742
e-meyer <edmundom@mx2.unisc.br>
2023-03-11 23:48:49 -0300

    remove unintended code from string-validators in introduction

--- python/introduction/string-validators.py
+++ python/introduction/string-validators.py
@@ -5,37 +5,3 @@ if __name__ == '__main__':
     print(any(c.isdigit() for c in s))
     print(any(c.islower() for c in s))
     print(any(c.isupper() for c in s))
-
-
-    #!/bin/python3
-
-import sys
-import os
-import urllib.request
-import json
-
-# Complete the function below.
-# https://jsonmock.hackerrank.com/api/countries/search?name=
-def  getCountries(s, p):
-    url = "https://jsonmock.hackerrank.com/api/countries/search?name="
-    
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        res = response.read()
-    
-    
-f = open(os.environ['OUTPUT_PATH'], 'w')
-    
-
-try:
-    _s = str(input())
-except:
-    _s = None
-
-
-_p = int(input());
-
-res = getCountries(_s, _p)
-f.write(str(res) + "\n")
-
-f.close()
